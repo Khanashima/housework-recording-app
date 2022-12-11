a428b0386260b97ee1aea7ae40aaea11c749cd57
Khanashima <kiiimii0129@gmail.com>
2022-12-11 20:25:36 +0900

    家事登録画面のガワを作成

---
+++ housework/lib/presentation/housework_register/housework_register_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HouseworkRegisterPage extends StatelessWidget {
+  const HouseworkRegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('家事登録画面'),
+      ),
+      body: const Center(
+        child: Text('家事登録画面'),
+      ),
+    );
+  }
+}
