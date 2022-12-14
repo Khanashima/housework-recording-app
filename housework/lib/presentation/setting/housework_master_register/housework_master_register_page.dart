145a152cc8cd74e8c13dca2bd4866041f391c008
Khanashima <kiiimii0129@gmail.com>
2022-12-15 01:11:30 +0900

    家事マスタ登録画面のガワ、ルーティングを作成

---
+++ housework/lib/presentation/setting/housework_master_register/housework_master_register_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HouseworkMasterRegisterPage extends StatelessWidget {
+  HouseworkMasterRegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('家事登録画面'),
+      ),
+      body: const Center(
+        child: Text("家事登録画面")
+      ),
+    );
+  }
+}
