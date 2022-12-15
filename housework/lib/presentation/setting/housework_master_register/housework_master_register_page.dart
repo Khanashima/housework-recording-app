25234d6c7b0f4b4d92d98870073493793a6b2c8e
Khanashima <kiiimii0129@gmail.com>
2022-12-16 01:12:32 +0900

    家事マスタ登録画面でテキストフィールドを表示

--- housework/lib/presentation/setting/housework_master_register/housework_master_register_page.dart
+++ housework/lib/presentation/setting/housework_master_register/housework_master_register_page.dart
@@ -2,16 +2,37 @@ import 'package:flutter/material.dart';
 
 class HouseworkMasterRegisterPage extends StatelessWidget {
   HouseworkMasterRegisterPage({super.key});
-
+  final _houseWorkTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('家事登録画面'),
-      ),
-      body: const Center(
-        child: Text("家事登録画面")
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('家事登録画面'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            TextField(
+              controller: _houseWorkTextController,
+              enabled: true,
+              // 入力数
+              maxLength: 10,
+              style: const TextStyle(color: Colors.black),
+              obscureText: false,
+              maxLines: 1,
+              decoration: const InputDecoration(
+                hintText: '家事名を入力してください',
+                labelText: '家事 *',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print(_houseWorkTextController.text);
+              },
+              child: Text('登録'),
+            )
+          ],
+        ));
   }
 }
